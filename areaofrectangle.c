afc8b21c934470c0b033bcd91890d30851e093e1
teju-nikam <nikamteju01@gmail.com>
2025-01-31 11:29:22 +0530

    Add files via upload

---
+++ areaofrectangle.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+	int l,b,area;
+	printf("Enter the length:");
+	scanf("%d",&l);
+	printf("Enter the breadth:");
+	scanf("%d",&b);
+	area=l*b;
+	printf("Area of rectangle:%d",area);
+	return 0;
+}
